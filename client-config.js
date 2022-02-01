3774612db65314cb742386d4bf19fd1f98350a56
jakengatchu <jngatchu@gmail.com>
2022-02-01 22:30:46 +0000

    initial commit

--- client-config.js
+++ client-config.js
@@ -1,5 +1,5 @@
 const clientConfig = {
     siteUrl:'http://localhost:3000',
-    graphqlUrl: 'http://seed.x10.mx/wp/graphql',
+    graphqlUrl: 'https://seed.x10.mx/wp/graphql',
 }
 module.exports = clientConfig;
